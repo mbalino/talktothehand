7bacade3302ab4585dd2ffb92522a8403d32e820
Martín Baliño <martin.balino@silogik.com>
2020-07-01 18:42:53 -0300

    Echo implementation with logging enabled. Extra samples (methods and routes)

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -32,11 +34,62 @@ func delete(w http.ResponseWriter, _ *http.Request) {
 	_, _ = w.Write([]byte(`{"message": "delete called"}`))
 }
 
-func notFound(w http.ResponseWriter, _ *http.Request) {
+func params(w http.ResponseWriter, r *http.Request) {
+
+	pathParams := mux.Vars(r)
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusNotFound)
-	_, _ = w.Write([]byte(`{"message": "not found"}`))
+
+	userID := -1
+
+	var err error
+
+	if val, ok := pathParams["userID"]; ok {
+
+		userID, err = strconv.Atoi(val)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte(`{"message": "need a number"}`))
+			return
+		}
+	}
+
+	commentID := -1
+	if val, ok := pathParams["commentID"]; ok {
+		commentID, err = strconv.Atoi(val)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte(`{"message": "need a number"}`))
+		}
+	}
+
+	query := r.URL.Query()
+	location := query.Get("location")
+
+	_, _ = w.Write([]byte(fmt.Sprintf(`{"userID": %d, "commentID": %d, "location": "%s" }`, userID, commentID, location)))
+
 }
+
+func echomsg(w http.ResponseWriter, r *http.Request) {
+
+	log.Println(r.URL)
+
+	pathParams := mux.Vars(r)
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	message := pathParams["message"]
+
+	if message != "" {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte(fmt.Sprintf(`{"message": "%s"}`, message)))
+
+		log.Printf(`{"message": "%s"}`, message)
+
+		return
+	}
+
+}
+
 func main() {
 
 	r := mux.NewRouter()
@@ -47,7 +100,9 @@ func main() {
 	api.HandleFunc("", post).Methods(http.MethodPost)
 	api.HandleFunc("", put).Methods(http.MethodPut)
 	api.HandleFunc("", delete).Methods(http.MethodDelete)
-	api.HandleFunc("", notFound)
+
+	api.HandleFunc("/user/{UserID}/comment/{commentID}", params).Methods(http.MethodGet)
+	api.HandleFunc("/echo/{message}", echomsg).Methods(http.MethodGet)
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 
